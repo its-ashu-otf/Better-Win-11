9c2168f8cb434f7e1ad6530be1e6bd40e8cfeb25
Ashutosh <ashutoshtips@gmail.com>
2024-06-04 21:42:04 +0530

    Update for Scripts

--- update_OEM_info.bat
+++ update_OEM_info.bat
@@ -1,32 +1,34 @@
 @echo off
 setlocal
-title Update OEM information
+title Update OEM Information
 
 REM Check if the script is run as administrator
 net session >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Error: This script must be run with administrative privileges.
-    echo Please run this script as an administrator and try again.
+    echo Error: Administrative privileges are required to run this script.
+    echo Please restart this script as an administrator.
     pause
     exit /b 1
 )
 
-echo OEM Info Editor By @its_ashu_otf
+echo Welcome to the OEM Info Editor by @its_ashu_otf
 
 REM Prompt the user to input company information
-set /p companyName="Enter company name: "
-set /p companyWebsite="Enter company website: "
-set /p supportContact="Enter support contact information: "
-set /p supportHours="Enter support hours: "
-set /p deviceModel="Enter the device model: "
+echo Please enter the following OEM details:
+set /p companyName="Company Name: "
+set /p companyWebsite="Company Website: "
+set /p supportContact="Support Contact Information: "
+set /p supportHours="Support Hours: "
+set /p deviceModel="Device Model: "
 
-REM Set the OEM information in the registry
+REM Update the OEM information in the registry
+echo Updating OEM information in the registry...
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v Manufacturer /d "%companyName%" /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v SupportURL /d "%companyWebsite%" /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v SupportPhone /d "%supportContact%" /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v SupportHours /d "%supportHours%" /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v Model /d "%deviceModel%" /f
 
-echo OEM information has been updated successfully.
+echo OEM information has been successfully updated.
 
 endlocal
