8854ea60636e2beb8c296f66f8d9ae0c17aa35f5
Ashutosh <ashutoshtips@gmail.com>
2024-06-26 01:50:32 +0530

    Update ultimateperformance.bat
    - Major Fixes for Laptops.
    - Little Refactor.

--- ultimateperformance.bat
+++ ultimateperformance.bat
@@ -6,18 +6,14 @@ REM Check if running with administrative privileges
     exit /b
 )
 
-REM Enable Ultimate Performance mode
-echo Enabling Ultimate Performance mode...
-
-REM Set power scheme to Ultimate Performance
-powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
-
 REM Modify registry to enable Ultimate Performance plan
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power" /v UltimatePowerScheme /t REG_SZ /d e9a42b02-d5df-448d-aa00-03f14749eb61 /f >nul 2>&1
+reg add HKLM\System\CurrentControlSet\Control\Power /v PlatformAoAcOverride /t REG_DWORD /d 0
 
-REM Apply the Ultimate Performance plan
-powercfg /setactive e9a42b02-d5df-448d-aa00-03f14749eb61
+REM Enable Ultimate Performance mode
+powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
 
-echo Ultimate Performance mode has been enabled.
+
+echo Please reboot your PC and now in Power Options Ultimate Performance Plan Should Be Visible.
 pause
 
